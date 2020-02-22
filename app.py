8c019b4120992f937cde90f0d51b474bed5eb359
philipstubbs13 <philipstubbs13@gmail.com>
2020-02-21 21:28:23 -0600

    awesome sauce

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
+        f"Welcome to the Climate App API!<br>"
+        f"Use this API if you dare...<br/>"
+        f"Here are the available routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
@@ -56,14 +58,16 @@ def precipitation():
     session = Session(engine)
 
     # Calculate the date 1 year ago from the last data point in the database.
-    last_measurement_data_point_tuple = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_measurement_data_point_tuple = session.query(
+        Measurement.date).order_by(Measurement.date.desc()).first()
     (latest_date, ) = last_measurement_data_point_tuple
     latest_date = dt.datetime.strptime(latest_date, '%Y-%m-%d')
     latest_date = latest_date.date()
     date_year_ago = latest_date - relativedelta(years=1)
 
     # Perform a query to retrieve the data and precipitation scores.
-    data_from_last_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= date_year_ago).all()
+    data_from_last_year = session.query(Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= date_year_ago).all()
 
     session.close()
 
@@ -71,12 +75,13 @@ def precipitation():
     all_precipication = []
     for date, prcp in data_from_last_year:
         if prcp != None:
-          precip_dict = {}
-          precip_dict[date] = prcp
-          all_precipication.append(precip_dict)
+            precip_dict = {}
+            precip_dict[date] = prcp
+            all_precipication.append(precip_dict)
 
     # Return the JSON representation of dictionary.
     return jsonify(all_precipication)
+
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -85,14 +90,16 @@ def tobs():
     session = Session(engine)
 
     # Calculate the date 1 year ago from the last data point in the database.
-    last_measurement_data_point_tuple = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_measurement_data_point_tuple = session.query(
+        Measurement.date).order_by(Measurement.date.desc()).first()
     (latest_date, ) = last_measurement_data_point_tuple
     latest_date = dt.datetime.strptime(latest_date, '%Y-%m-%d')
     latest_date = latest_date.date()
     date_year_ago = latest_date - relativedelta(years=1)
 
     # Perform a query to retrieve the data and temperature scores.
-    data_from_last_year = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= date_year_ago).all()
+    data_from_last_year = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.date >= date_year_ago).all()
 
     session.close()
 
@@ -100,11 +107,12 @@ def tobs():
     all_temperatures = []
     for date, temp in data_from_last_year:
         if temp != None:
-          temp_dict = {}
-          temp_dict[date] = temp
-          all_temperatures.append(temp_dict)
+            temp_dict = {}
+            temp_dict[date] = temp
+            all_temperatures.append(temp_dict)
     # Return the JSON representation of dictionary.
     return jsonify(all_temperatures)
+
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -113,23 +121,62 @@ def stations():
     session = Session(engine)
 
     # Query for stations.
-    stations = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+    stations = session.query(Station.station, Station.name,
+                             Station.latitude, Station.longitude, Station.elevation).all()
 
     session.close()
 
     # Convert the query results to a dictionary.
     all_stations = []
     for station, name, latitude, longitude, elevation in stations:
-      station_dict = {}
-      station_dict["station"] = station
-      station_dict["name"] = name
-      station_dict["latitude"] = latitude
-      station_dict["longitude"] = longitude
-      station_dict["elevation"] = elevation
-      all_stations.append(station_dict)
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        all_stations.append(station_dict)
 
     # Return the JSON representation of dictionary.
     return jsonify(all_stations)
+
+
+@app.route('/api/v1.0/<start>', defaults={'end': None})
+@app.route("/api/v1.0/<start>/<end>")
+def determine_temps_for_date_range(start, end):
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."""
+    """When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date."""
+    """When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive."""
+    # Create our session (link) from Python to the DB.
+    session = Session(engine)
+
+    # If we have both a start date and an end date.
+    if end != None:
+        temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).filter(
+            Measurement.date <= end).all()
+    # If we only have a start date.
+    else:
+        temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).all()
+
+    session.close()
+
+    # Convert the query results to a list.
+    temperature_list = []
+    no_temperature_data = False
+    for min_temp, avg_temp, max_temp in temperature_data:
+        if min_temp == None or avg_temp == None or max_temp == None:
+            no_temperature_data = True
+        temperature_list.append(min_temp)
+        temperature_list.append(avg_temp)
+        temperature_list.append(max_temp)
+    # Return the JSON representation of dictionary.
+    if no_temperature_data == True:
+        return f"No temperature data found for the given date range. Try another date range."
+    else:
+        return jsonify(temperature_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
